c0abedcbc145a408361be7be5672d7c20ff12a40
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/IrcPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/IrcPlugin.pm
+++ lib/TWiki/Plugins/IrcPlugin.pm
@@ -42,11 +42,12 @@ $pluginName = 'IrcPlugin';
 ################################################################################
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.026 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1.026 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
@@ -55,26 +56,38 @@ sub initPlugin {
 }
 
 sub afterSaveHandler {
+
     # $text = $[0]
-    my ( $topic, $web, $error, $meta ) = @_[1..4];
+    my ( $topic, $web, $error, $meta ) = @_[ 1 .. 4 ];
 
-    TWiki::Func::writeDebug( "- ${pluginName}::afterSaveHandler( ${web}.${topic} )" ) if $debug;
+    TWiki::Func::writeDebug(
+        "- ${pluginName}::afterSaveHandler( ${web}.${topic} )")
+      if $debug;
 
-    my $topicInfo = $meta->get( 'TOPICINFO' );
-    # Strip the front 1. part of rcs version numbers to get simple revision numbers
+    my $topicInfo = $meta->get('TOPICINFO');
+
+ # Strip the front 1. part of rcs version numbers to get simple revision numbers
     ( my $version = $topicInfo->{version} ) =~ s/^[\d]+\.//;
 
-    _writeIrc({ newconn => {
-	Server => TWiki::Func::getPluginPreferencesValue( 'SERVER' ) || 'localhost',
-	Port => TWiki::Func::getPluginPreferencesValue( 'PORT' ) || '6667',
-	Nick => 'TWikiIrcPlugin' || TWiki::Func::getPluginPreferencesValue( 'NICK' ) || 'TWikiIrcPlugin',
-#	Ircname  => 'This bot brought to you by Net::IRC.',
-#	Username => 'TWikiIrcPlugin',
-    },
+    _writeIrc(
+        {
+            newconn => {
+                Server => TWiki::Func::getPluginPreferencesValue('SERVER')
+                  || 'localhost',
+                Port => TWiki::Func::getPluginPreferencesValue('PORT')
+                  || '6667',
+                Nick => 'TWikiIrcPlugin'
+                  || TWiki::Func::getPluginPreferencesValue('NICK')
+                  || 'TWikiIrcPlugin',
+
+                #	Ircname  => 'This bot brought to you by Net::IRC.',
+                #	Username => 'TWikiIrcPlugin',
+            },
                 msg => TWiki::Func::getScriptUrl( $web, $topic, 'view' ) . ' '
-                    . ( $version == 1 ? 'created' : "updated to r$version" )
-                    . " by $topicInfo->{author}",
-	    });
+              . ( $version == 1 ? 'created' : "updated to r$version" )
+              . " by $topicInfo->{author}",
+        }
+    );
 
     # more code here
 }
@@ -84,27 +97,30 @@ my $looping;
 
 sub _writeIrc {
     my $p = shift;
-    print STDERR "_writeIrc:".Data::Dumper::Dumper( $p );
+    print STDERR "_writeIrc:" . Data::Dumper::Dumper($p);
 
     my $irc = Net::IRC->new() or die $!;
-    my $conn = $irc->newconn( %{$p->{newconn}} );
+    my $conn = $irc->newconn( %{ $p->{newconn} } );
+
     # SMELL: make exception
     if ( !$conn ) {
-	# SMELL: write to queue
-	print STDERR "IrcPlugin: Can't connect to IRC server : " . Data::Dumper::Dumper( $p );
-	return;
+
+        # SMELL: write to queue
+        print STDERR "IrcPlugin: Can't connect to IRC server : "
+          . Data::Dumper::Dumper($p);
+        return;
     }
     $conn->{msg} = $p->{msg};
 
-    $conn->add_handler('msg', \&on_msg);
+    $conn->add_handler( 'msg', \&on_msg );
 
-    $conn->add_global_handler([ 251,252,253,254,302,255 ], \&on_init);
-    $conn->add_global_handler(376, \&on_connect);
-    $conn->add_global_handler(433, \&on_nick_taken);
+    $conn->add_global_handler( [ 251, 252, 253, 254, 302, 255 ], \&on_init );
+    $conn->add_global_handler( 376, \&on_connect );
+    $conn->add_global_handler( 433, \&on_nick_taken );
 
-#    $irc->start();
-    for ( $looping = 1; $looping; ) {
-	$irc->do_one_loop();
+    #    $irc->start();
+    for ( $looping = 1 ; $looping ; ) {
+        $irc->do_one_loop();
     }
 }
 
@@ -114,21 +130,22 @@ sub _writeIrc {
 sub on_connect {
     my $self = shift;
 
-    my $CHANNEL = TWiki::Func::getPluginPreferencesValue( 'CHANNEL' ) || 'test';
-    $self->join( $CHANNEL );
-#    print STDERR "nick=[" . $self->nick . "]\n";
+    my $CHANNEL = TWiki::Func::getPluginPreferencesValue('CHANNEL') || 'test';
+    $self->join($CHANNEL);
+
+    #    print STDERR "nick=[" . $self->nick . "]\n";
     foreach ( $CHANNEL, $self->nick ) {
-	$self->privmsg( $_, $self->{msg} );
+        $self->privmsg( $_, $self->{msg} );
     }
 }
 
 # Handles some messages you get when you connect
 sub on_init {
-    my ($self, $event) = @_;
-    my (@args) = ($event->args);
-    shift (@args);
-    
-#    print "*** @args\n";
+    my ( $self, $event ) = @_;
+    my (@args) = ( $event->args );
+    shift(@args);
+
+    #    print "*** @args\n";
 }
 
 # Change our nick if someone stole it.
@@ -136,16 +153,16 @@ sub on_nick_taken {
     my ($self) = shift;
 
     print STDERR "argh! nick [" . $self->nick . "] taken!!!\n";
-    $self->nick(substr($self->nick, -1) . substr($self->nick, 0, 8));
+    $self->nick( substr( $self->nick, -1 ) . substr( $self->nick, 0, 8 ) );
 }
 
 sub on_msg {
-    my ($self, $event) = @_;
+    my ( $self, $event ) = @_;
     my ($nick) = $event->nick;
 
 #    print "*$nick*  ", ($event->args), "\n";
-    # we've received the message we broadcasted; we're done
-    # SMELL: unless some talked to us, and we didn't actually get back the message back yet...
+# we've received the message we broadcasted; we're done
+# SMELL: unless some talked to us, and we didn't actually get back the message back yet...
     $looping = 0;
 }
 
--- test/unit/IrcPlugin/IrcPluginSuite.pm
+++ test/unit/IrcPlugin/IrcPluginSuite.pm
@@ -3,8 +3,8 @@ package IrcPluginSuite;
 use Unit::TestSuite;
 our @ISA = qw( Unit::TestSuite );
 
-sub name { 'IrcPluginSuite' };
+sub name { 'IrcPluginSuite' }
 
-sub include_tests { qw(IrcPluginTests) };
+sub include_tests { qw(IrcPluginTests) }
 
 1;
